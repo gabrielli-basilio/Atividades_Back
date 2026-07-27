1cdc3340b13041b41cb8a94da02420c0d7ef137f
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-07-26 22:43:57 -0300

    Atividade_02

--- Atividades.php
+++ Atividades.php
@@ -11,4 +11,21 @@
     } else {
         echo "Você é menor de idade. \n";
     }
+    
+
+    // 2. Crie uma variável $dinheiro.
+
+    $dinheiro = 2000;
+
+    if ($dinheiro <= 1000) {
+        echo "Pobre";
+    } else if ($dinheiro <= 5000) {
+        echo "Classe Média";
+    } else if ($dinheiro <= 50000) {
+        echo "Riquinho";
+    } else if ($dinheiro <= 1000000) {
+        echo "Ricão";
+    } else {
+        echo "Elon Musk";
+    }
 ?>
