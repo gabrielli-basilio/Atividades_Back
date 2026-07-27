62bea2308f623f8a6a1c17cb4be5aab84be26512
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-07-26 22:27:23 -0300

    Atividade_01

---
+++ Atividades.php
@@ -0,0 +1,14 @@
+<?php
+
+// IF e ELSE:
+
+    // 1. Crie uma variável $idade.
+
+    $idade = 12;
+
+    if ($idade >= 18) {
+        echo "Você é maior de idade. \n";
+    } else {
+        echo "Você é menor de idade. \n";
+    }
+?>
